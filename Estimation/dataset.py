354a38de38e347cdd4f0c870af032b5a2bf8c9ad
b1017173 <g2121034@fun.ac.jp>
2021-04-27 02:06:21 +0900

    もう救えんけど，なんかできたわ

--- Estimation/dataset.py
+++ Estimation/dataset.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.utils import Bunch
+from sklearn.model_selection import train_test_split
+from sklearn import svm, neighbors, naive_bayes, metrics
+
 
 class Dataset():
     def __init__(self, attributeList:list, trainDataList:np.ndarray, testDataList:np.ndarray):
@@ -9,6 +13,53 @@ class Dataset():
     
     def convertToScikitDataset(self):
         self.pandasData = pd.DataFrame(np.concatenate([self.trainDataList, self.trainDataList]), columns=[attr.name for attr in self.attributeList])
+        # print(self.pandasData.shape)
+        # print(self.pandasData.keys)
+        _scikitDataset = Bunch()
+        _scikitDataset['target'] = self.pandasData['class']
+        _scikitDataset['data'] = self.pandasData.loc[:, :self.pandasData.columns.values[-2]]
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(_scikitDataset['data'], _scikitDataset['target'], random_state=0)
+        # print("X_train shape:", self.X_train.shape)
+        # print("X_test shape:", self.X_test.shape)
 
-        print(self.pandasData)
-        return 
+        return self.X_train, self.X_test, self.Y_train, self.Y_test
+
+    def scikitLinearSVC(self, X_train, X_test, Y_train, Y_test):
+        print("--- LinearSVC ---")
+        _LSVC = svm.LinearSVC(max_iter=10000)
+        _LSVC.fit(X_train, Y_train)
+        _accuracy = _LSVC.score(X_test, Y_test)
+        print("正答率", _accuracy)
+
+        print("詳細結果")
+        print(metrics.classification_report(Y_test, _LSVC.predict(X_test)))
+    
+    def scikitKNeighborsClassifier(self, X_train, X_test, Y_train, Y_test):
+        print("--- KNeighborsClassifier ---")
+        _KNC = neighbors.KNeighborsClassifier()
+        _KNC.fit(X_train, Y_train)
+        _accuracy = _KNC.score(X_test, Y_test)
+        print("正答率", _accuracy)
+
+        print("詳細結果")
+        print(metrics.classification_report(Y_test, _KNC.predict(X_test)))
+    
+    def scikitSVC(self, X_train, X_test, Y_train, Y_test):
+        print("--- SVC ---")
+        _SVC = svm.SVC(max_iter=10000)
+        _SVC.fit(X_train, Y_train)
+        _accuracy = _SVC.score(X_test, Y_test)
+        print("正答率", _accuracy)
+
+        print("詳細結果")
+        print(metrics.classification_report(Y_test, _SVC.predict(X_test)))
+    
+    def scikitNaiveBayes(self, X_train, X_test, Y_train, Y_test):
+        print("--- Naive Bayes ---")
+        _GNB = naive_bayes.GaussianNB()
+        _GNB.fit(X_train, Y_train)
+        _accuracy = _GNB.score(X_test, Y_test)
+        print("正答率", _accuracy)
+
+        print("詳細結果")
+        print(metrics.classification_report(Y_test, _GNB.predict(X_test)))
--- Estimation/main.py
+++ Estimation/main.py
@@ -1,19 +1,31 @@
 from attribute import Attribute
 from dataset import Dataset
 import numpy as np
+import pandas as pd
+
+def machineLearning(dataset):
+    X_train, X_test, Y_train, Y_test = dataset.convertToScikitDataset()
+    dataset.scikitLinearSVC(X_train, X_test, Y_train, Y_test)
+    dataset.scikitKNeighborsClassifier(X_train, X_test, Y_train, Y_test)
+    dataset.scikitSVC(X_train, X_test, Y_train, Y_test)
+    dataset.scikitNaiveBayes(X_train, X_test, Y_train, Y_test)
 
 def readArff():
     # train_path = sys.argv[1]
     # test_path = sys.argv[2]
-    train_path = 'data/segment_train.arff'
-    test_path = 'data/segment_test.arff'
+    # train_path = 'data/segment_train.arff'
+    # test_path = 'data/segment_test.arff'
+    train_path = 'data/excitement_train.arff'
+    test_path = 'data/excitement_test.arff'
+    # train_path = 'data/estimation_train.arff'
+    # test_path = 'data/estimation_test.arff'
     attributeList, trainDataList = readTrainArff(train_path)
     testDataList = readTestArff(test_path)
-    print('attributeList:', attributeList)
-    print('trainDataList:', trainDataList)
-    print('testDataList:', testDataList)
+    # print('attributeList:', attributeList)
+    # print('trainDataList:', trainDataList)
+    # print('testDataList:', testDataList)
     dataset = Dataset(attributeList, trainDataList, testDataList)
-    dataset.convertToScikitDataset()
+    return dataset
 
 def readTrainArff(train_path:str):
     attributeList = []
@@ -40,6 +52,9 @@ def readTrainArff(train_path:str):
                 dataList = [line.split(',')]
             else:
                 dataList = np.insert(dataList, 0, line.split(','), axis=0)
+    dataList = np.where(dataList == '?', np.NaN, dataList)                              # 欠損値をnumpy用に置換
+    row_contain_nan = ~np.isnan(np.delete(dataList, -1, 1).astype(float)).any(axis=1)   # 欠損値のある行の検知
+    dataList = dataList[row_contain_nan]                                                # 欠損値のある行を消去
     return attributeList, dataList
 
 def readTestArff(test_path:str):
@@ -52,10 +67,14 @@ def readTestArff(test_path:str):
                 dataList = [line.split(',')]
             else:
                 dataList = np.insert(dataList, 0, line.split(','), axis=0)
+    dataList = np.where(dataList == '?', np.NaN, dataList)
+    row_contain_nan = ~np.isnan(np.delete(dataList, -1, 1).astype(float)).any(axis=1)
+    dataList = dataList[row_contain_nan]   
     return dataList
 
 def main():
-    readArff()
+    dataset = readArff()
+    machineLearning(dataset)
 
 if __name__ == '__main__':
     main()
